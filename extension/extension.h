77dde09480e819b45b712598022954de62137b4a
thesupremecommander <thesupremecmdr@gmail.com>
2014-07-27 17:50:25 -0400

    Update parameter names.

--- extension/extension.h
+++ extension/extension.h
@@ -6,16 +6,16 @@
 class CompCtrl : public SDKExtension
 {
 public:
-	//virtual bool SDK_OnLoad(char *error, size_t maxlen, bool late);
+	//virtual bool SDK_OnLoad(char *error, size_t maxlength, bool late);
 	//virtual void SDK_OnUnload();
 	//virtual void SDK_OnAllLoaded();
 	//virtual void SDK_OnPauseChange(bool paused);
-	//virtual bool QueryRunning(char *error, size_t maxlen);
+	//virtual bool QueryRunning(char *error, size_t maxlength);
 public:
 #if defined SMEXT_CONF_METAMOD
-	//virtual bool SDK_OnMetamodLoad(ISmmAPI *ismm, char *error, size_t maxlen, bool late);
-	//virtual bool SDK_OnMetamodUnload(char *error, size_t maxlen);
-	//virtual bool SDK_OnMetamodPauseChange(bool paused, char *error, size_t maxlen);
+	//virtual bool SDK_OnMetamodLoad(ISmmAPI *ismm, char *error, size_t maxlength, bool late);
+	//virtual bool SDK_OnMetamodUnload(char *error, size_t maxlength);
+	//virtual bool SDK_OnMetamodPauseChange(bool paused, char *error, size_t maxlength);
 #endif
 };
 
