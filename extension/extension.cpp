eb86bb7e5841bea45709e74ca95ece0ca839908f
thesupremecommander <thesupremecmdr@gmail.com>
2014-07-27 18:39:11 -0400

    Add gamedata.

--- extension/extension.cpp
+++ extension/extension.cpp
@@ -4,11 +4,23 @@ CompCtrl g_CompCtrl;		/**< Global singleton for extension's main interface */
 
 SMEXT_LINK(&g_CompCtrl);
 
-ISDKHooks *g_pSDKHooks = NULL;
+IGameConfig *g_pGameConfig = nullptr;
+ISDKHooks *g_pSDKHooks = nullptr;
 
 bool CompCtrl::SDK_OnLoad(char *error, size_t maxlength, bool late)
 {
 	sharesys->AddDependency(myself, "sdkhooks.ext", true, true);
+
+	char *gameConfigError = new char[255];
+	if (!gameconfs->LoadGameConfigFile("compctrl", &g_pGameConfig, gameConfigError, sizeof(gameConfigError)))
+	{
+		if (gameConfigError[0])
+		{
+			V_snprintf(error, maxlength, "Could not read compctrl.txt: %s", gameConfigError);
+		}
+		return false;
+	}
+
 	return true;
 }
 
