2378be8ad7301ef39422f8a77b5bee9f6ad7bb38
thesupremecommander <thesupremecmdr@gmail.com>
2014-07-27 17:52:11 -0400

    Add SDKHooks as a dependency.

--- extension/extension.cpp
+++ extension/extension.cpp
@@ -3,3 +3,22 @@
 CompCtrl g_CompCtrl;		/**< Global singleton for extension's main interface */
 
 SMEXT_LINK(&g_CompCtrl);
+
+ISDKHooks *g_pSDKHooks = NULL;
+
+bool CompCtrl::SDK_OnLoad(char *error, size_t maxlength, bool late)
+{
+	sharesys->AddDependency(myself, "sdkhooks.ext", true, true);
+	return true;
+}
+
+void CompCtrl::SDK_OnAllLoaded()
+{
+	SM_GET_LATE_IFACE(SDKHOOKS, g_pSDKHooks);
+}
+
+bool CompCtrl::QueryRunning(char *error, size_t maxlength)
+{
+	SM_CHECK_IFACE(SDKHOOKS, g_pSDKHooks);
+	return true;
+}
--- extension/extension.h
+++ extension/extension.h
@@ -2,15 +2,16 @@
 #define _INCLUDE_COMPCTRL_H_
 
 #include "smsdk_ext.h"
+#include "ISDKHooks.h"
 
 class CompCtrl : public SDKExtension
 {
 public:
-	//virtual bool SDK_OnLoad(char *error, size_t maxlength, bool late);
+	virtual bool SDK_OnLoad(char *error, size_t maxlength, bool late);
 	//virtual void SDK_OnUnload();
-	//virtual void SDK_OnAllLoaded();
+	virtual void SDK_OnAllLoaded();
 	//virtual void SDK_OnPauseChange(bool paused);
-	//virtual bool QueryRunning(char *error, size_t maxlength);
+	virtual bool QueryRunning(char *error, size_t maxlength);
 public:
 #if defined SMEXT_CONF_METAMOD
 	//virtual bool SDK_OnMetamodLoad(ISmmAPI *ismm, char *error, size_t maxlength, bool late);
@@ -18,5 +19,7 @@ public:
 	//virtual bool SDK_OnMetamodPauseChange(bool paused, char *error, size_t maxlength);
 #endif
 };
+
+extern ISDKHooks *g_pSDKHooks;
 
 #endif // _INCLUDE_COMPCTRL_H_
